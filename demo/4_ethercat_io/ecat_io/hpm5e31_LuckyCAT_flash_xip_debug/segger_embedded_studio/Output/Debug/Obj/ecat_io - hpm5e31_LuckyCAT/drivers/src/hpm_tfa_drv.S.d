be3c1f7ae541c52c83f5ff6b7a54ebb7f74f02f9
flose <seeyounextsun@outlook.com>
2025-07-16 20:25:09 +0800

    删除生成的工程目录，缩小源码体积

--- demo/4_ethercat_io/ecat_io/hpm5e31_LuckyCAT_flash_xip_debug/segger_embedded_studio/Output/Debug/Obj/ecat_io - hpm5e31_LuckyCAT/drivers/src/hpm_tfa_drv.S.d
+++
@@ -1,2 +0,0 @@
-Output/Debug/Obj/ecat_io\ -\ hpm5e31_LuckyCAT/drivers/src/hpm_tfa_drv.S.o: \
-  C:\Env\hpm_env\sdk_env\hpm_sdk\drivers\src\hpm_tfa_drv.S
--- demo/4_ethercat_io/ecat_io/hpm5e31_LuckyCAT_flash_xip_debug/segger_embedded_studio/Output/Debug/Obj/ecat_io - hpm5e31_LuckyCAT/utils/hpm_crc32.c.d
+++
@@ -1,8 +0,0 @@
-Output/Debug/Obj/ecat_io\ -\ hpm5e31_LuckyCAT/utils/hpm_crc32.c.o: \
- C:\Env\hpm_env\sdk_env\hpm_sdk\utils\hpm_crc32.c \
- C:\Env\hpm_env\sdk_env\hpm_sdk\utils\hpm_crc32.h \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ 8.24/include/stdint.h \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ 8.24/include/__SEGGER_RTL.h \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ 8.24/include/__SEGGER_RTL_ConfDefaults.h \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ 8.24/include/__SEGGER_RTL_Conf.h \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ 8.24/include/__SEGGER_RTL_RISCV_Conf.h
